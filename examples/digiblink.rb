ef9c9d05bbcb20ca66dab51e34043ea052db2083
Jenna Fox <a@creativepony.com>
2013-01-06 12:25:08 +1100

    examples: digiblink updated to use colorist rubygem and be awesomer and much shorter!

--- examples/digiblink.rb
+++ examples/digiblink.rb
@@ -1,33 +1,17 @@
 # A little example program for driving the digirgb example code
-
+# needs colorist rubygem to parse colors
 require 'digiusb'
+require 'colorist'
 
 light = DigiUSB.sparks.last # get the spark most recently plugged in (or maybe just a random one? not sure)
+raise "Couldn't find a DigiUSB device to talk to" if light.nil?
 
-# some colour strings to lookup
-color_table = {
-  red: [255, 0, 0],
-  green: [0, 255, 0],
-  blue: [0, 0, 255],
-  yellow: [255, 255, 0],
-  aqua: [0, 255, 255],
-  violet: [255, 0, 255],
-  purple: [255, 0, 255],
-  black: [0, 0, 0],
-  grey: [127, 127, 127],
-  white: [255, 255, 255]
-}
-
-if color_table.has_key? ARGV.first.to_sym
-  color = color_table[ARGV.first.to_sym]
-else
-  color = ARGV.map { |string| string.to_i }
-end
+color = ARGV.first.to_color
 
 # send out each byte
-light.putc 115 # start character 's'
-light.putc color[0]
-light.putc color[1]
-light.putc color[2]
+light.putc 's' # start character
+light.putc color.r
+light.putc color.g
+light.putc color.b
 
-puts "Light should now be #{ARGV.join ':'}"
+puts "Light is now #{ARGV.first}"
