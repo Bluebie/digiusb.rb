18429352f6bf445e8233873cef782de12501e3bc
Jenna Fox <a@creativepony.com>
2013-01-07 14:41:13 +1100

    examples: digiblink program is now way shorter!

--- examples/digiblink.rb
+++ examples/digiblink.rb
@@ -1,17 +1,8 @@
 # A little example program for driving the digirgb example code
-# needs colorist rubygem to parse colors
-require 'digiusb'
-require 'colorist'
+require 'digiusb/digiblink'
 
 light = DigiUSB.sparks.last # get the spark most recently plugged in (or maybe just a random one? not sure)
 raise "Couldn't find a DigiUSB device to talk to" if light.nil?
 
-color = ARGV.first.to_color
-
-# send out each byte
-light.putc 's' # start character
-light.putc color.r
-light.putc color.g
-light.putc color.b
-
+light.color = ARGV.first
 puts "Light is now #{ARGV.first}"
